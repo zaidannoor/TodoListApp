6a60b42f7197d6bbfcbb529cf627292e41ca86b1
Zaidan Noor <zaidannoor06@gmail.com>
2022-08-16 20:31:56 +0700

    Menambahkan Fitur list tidak boleh sama

--- js/script.js
+++ js/script.js
@@ -19,37 +19,56 @@ addTaskForm.addEventListener('submit', function(event) {
     // mendapatkan nilai form addTask
     let task = addTask.value;
 
-    // push nilai ke dalam variabel myTask
-    myTask.push(task);
+    // mengecek apakah task yang diinputkan sudah ada dalam array atau belum
+    if(myTask.find(tugas => tugas == task)){
+      // menampilkan alert kesalahan
+      alert(`${task} sudah ada dalam list`);
+      
+    }else{
+      // push nilai ke dalam variabel myTask
+      myTask.push(task);
 
-    // melakukan looping untuk menampilkan list task
-    let cards = '';
-    myTask.forEach(task => {
-        cards += `<div class="col-3 my-3 text-center">
-        <div class="card rounded-pill bg-dark text-light">
-            <div class="card-body d-flex justify-content-center">
-              ${task} 
-              <div class="ceklis mx-3">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="28" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
-                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                    <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
-                  </svg>
+      // melakukan looping untuk menampilkan list task
+      let cards = '';
+      myTask.forEach(task => {
+        cards += `
+        <div class="col-4 my-3 text-center">
+          <div class="card rounded-pill bg-dark text-light">
+              <div class="card-body d-flex justify-content-center">
+                  ${task} 
+                  <div class="ceklis mx-3">
+                      <svg xmlns="http://www.w3.org/2000/svg" width="28" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                        <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
+                      </svg>
+                  </div>
+                  <div class="delete">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="28" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
+                      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
+                      <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                    </svg>
+                  </div>  
               </div>
-              <div class="delete">
-                <svg xmlns="http://www.w3.org/2000/svg" width="28" fill="currentColor" class="bi bi-x-circle" viewBox="0 0 16 16">
-                  <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                </svg>
-              </div>  
-            </div>
-        </div>
-    </div>`
+          </div>
+        </div>`
         
         document.querySelector("main .list-container").innerHTML = cards;
         //$(".list-container").html(cards);
-        console.log(task);
     });
-    console.log(myTask);
+    }
+
+    
+
+    
     
 });
 
+let myArr = ['zaidan' , 'noor'];
+if(!myArr.find(name => name == 'zaidan')){
+  console.log('tidak ada');
+}
+
+  
+else{
+  console.log('ada');
+}
