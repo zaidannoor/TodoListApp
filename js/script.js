d81de56e2930662ff40fab7d19402b6db4fb0898
Zaidan Noor <zaidannoor06@gmail.com>
2022-08-17 11:39:56 +0700

    mengubah addeventlistener menjadi onclick

--- js/script.js
+++ js/script.js
@@ -22,9 +22,8 @@ let myTask =[];
 // variabel untuk menghitung iterasi
 let iteration = 0;
 
-// event saat form task disubmit
-addTaskForm.addEventListener('submit', function(event) {
-    //event.preventDefault();
+function add(){
+  //event.preventDefault();
     // mendapatkan nilai form addTask
     let task = addTask.value;
 
@@ -38,7 +37,6 @@ addTaskForm.addEventListener('submit', function(event) {
       myTask.push(task);
 
       // melakukan looping untuk menampilkan list task
-      
       let cards = '';
       myTask.forEach(task => {
         cards += `
@@ -47,7 +45,7 @@ addTaskForm.addEventListener('submit', function(event) {
               <div id="${iteration}" class="card-body d-flex justify-content-center">
                   ${task} 
                   <div class="ceklis mx-3">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="28" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
+                      <svg onclick="tes()" xmlns="http://www.w3.org/2000/svg" width="28" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16">
                         <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
                         <path d="M10.97 4.97a.235.235 0 0 0-.02.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05z"/>
                       </svg>
@@ -68,11 +66,11 @@ addTaskForm.addEventListener('submit', function(event) {
         deleteButton = document.querySelectorAll('main .list-container .delete svg');
     });
     } 
-});
-
+}
 
 const tes = () => {
-  checkButton.forEach(cb => cb.addEventListener('click', () => console.log('tes')))
+  console.log('tes');
+  //checkButton.forEach(cb => cb.addEventListener('click', () => console.log('tes')))
 };
 
 // function addEventListenerList(list, event, fn) {
@@ -82,7 +80,7 @@ const tes = () => {
 // }
 
 //addEventListenerList(checkButton,'click',() => console.log('tes'));
-tes();
+
 
 
   
